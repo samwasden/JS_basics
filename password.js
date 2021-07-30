3c409f93d6a36e2d56da0480f8b69a08881d5230
Sam Wasden <samwasden@outlook.com>
2021-07-30 11:03:23 -0600

    added more criteria for password.js

--- password.js
+++ password.js
@@ -5,13 +5,48 @@ const reader = readline.createInterface({
   })
 
 console.log("Welcome to the password validator tool!");
+console.log()
+console.log("Password Criteria \nMust be at least 10 characters long\nHave a minimum of 1 special character\nBe at least 1 Upper case letter\nHave no spaces")
 
-reader.question("Please enter your password: ", function(answer) {
-    console.log(answer)
-    if (answer.length >= 10) {
-        console.log("You have chosen an acceptable password.")
-    } else {
-        console.log("Please make sure your password meets the required criteria.")
-    }
-    reader.close()
-});
+let valid = false
+
+function getPassword() {
+    reader.question("Please enter your password: ", function(answer) {
+        let specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "<", ">", ",", ".", "/", "?", ":", '"', "'", ";", "{", "}", "[", "]", "|"]
+        let invalid = "This password is invalid. Please read criteria."
+        let accepted = "This password meets all the criteria and is valid."
+        let hasCaps = false
+        let hasSpecial = false
+        let noSpaces = true
+        if (answer.length >= 10) {
+            let answerArr = answer.split("");
+            for (let i=0; i<answerArr.length; i++) {
+                if (isNaN(answerArr[i])) {
+                    if (specialChars.includes(answerArr[i])) {
+                        hasSpecial = true;
+                    } else if (answerArr[i].toUpperCase() === answerArr[i]) {
+                        hasCaps = true
+                    } 
+                } else {
+                    if (answerArr[i] === " ") {
+                        noSpaces = false
+                    }
+                }
+            }
+        }
+        if (hasCaps && hasSpecial && noSpaces) {
+            valid = true
+            console.log()
+            console.log(accepted)
+            console.log()
+            reader.close() 
+        } else {
+            console.log()
+            console.log(invalid) 
+            console.log()
+            reader.close()
+        } 
+    });
+}
+
+getPassword()
