e1b3e4f90dbfe156084de98097539fa80359f32a
Sam Wasden <samwasden@outlook.com>
2021-07-30 11:31:01 -0600

    FINAL

--- password.js
+++ password.js
@@ -10,10 +10,26 @@ console.log("Password Criteria \nMust be at least 10 characters long\nHave a min
 
 let valid = false
 
+let congrats = `
+          _           __ 
+   ____  (_)_______  / / 
+  / __ \ / / ___/ _ \ / / 
+ / / / / / /__/  __/_/ 
+/_/ /_/_/\ ___/\ ___(_)
+`
+
+let failure = `
+        __                      _ __
+   ____/ / ___  _____ ___ ____ (_) /_ 
+  / __  / __ '/ __ ' __ / __  / / __/ 
+ / /_/ / /_/ / / / / / / / / / / /_
+  __,_/\ __,_/_/ /_/ /_/_/ /_/_/\ __/ 
+  `
+
 function getPassword() {
     reader.question("Please enter your password: ", function(answer) {
         let specialChars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", "<", ">", ",", ".", "/", "?", ":", '"', "'", ";", "{", "}", "[", "]", "|"]
-        let invalid = "This password is invalid. Please read criteria."
+        let invalid = "This password is invalid. Come on the criteria is not that hard."
         let accepted = "This password meets all the criteria and is valid."
         let hasCaps = false
         let hasSpecial = false
@@ -36,12 +52,12 @@ function getPassword() {
         }
         if (hasCaps && hasSpecial && noSpaces) {
             valid = true
-            console.log()
+            console.log(congrats)
             console.log(accepted)
             console.log()
             reader.close() 
         } else {
-            console.log()
+            console.log(failure)
             console.log(invalid) 
             console.log()
             reader.close()
